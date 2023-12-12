70e761b625dbc439e5331e98e385b63a5761aff1
okjuan <jcgallegdup@gmail.com>
2023-12-12 00:06:02 -0800

    Tweak indentation in script file

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var sliders = document.getElementsByClassName('slider');
 for (var i = 0; i < sliders.length; i++) {
-sliders[i].oninput = function() {
-    document.getElementById(this.id + '-value').textContent = this.value;
-    console.log(this.id + ' value: ' + this.value);
-}
-// Trigger the event manually to display the initial value
-sliders[i].oninput();
+    sliders[i].oninput = function() {
+            document.getElementById(this.id + '-value').textContent = this.value;
+            console.log(this.id + ' value: ' + this.value);
+    }
+    // Trigger the event manually to display the initial value
+    sliders[i].oninput();
 }
