4e47438837216c2fdedb49b8e4fdf23f0eb338d0
okjuan <jcgallegdup@gmail.com>
2023-12-12 00:08:54 -0800

    Fix bug in script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var sliders = document.getElementsByClassName('slider');
 for (var i = 0; i < sliders.length; i++) {
     sliders[i].oninput = function() {
-            document.getElementById(this.id + '-value').textContent = this.value;
+            document.getElementById(this.id).textContent = this.value;
             console.log(this.id + ' value: ' + this.value);
     }
     // Trigger the event manually to display the initial value
