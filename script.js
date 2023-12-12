5ebfd5478a9005d85d7027913a2ce9caa3304c13
okjuan <jcgallegdup@gmail.com>
2023-12-12 00:16:56 -0800

    Set up Spotify Webplayback SDK without auth set up (works if you paste auth token retrieved from Spotify's Getting Started with Web Playback SDK)

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = '[My access token]';
+    const player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb(token); },
+      volume: 0.5
+    });
+
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+  });
+
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+
+  player.addListener('initialization_error', ({ message }) => {
+    console.error(message);
+  });
+
+  player.addListener('authentication_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.addListener('account_error', ({ message }) => {
+      console.error(message);
+  });
+
+  player.connect();
+
+  document.getElementById('play-pause-button').onclick = function() {
+    player.togglePlay();
+    console.log("play/pause button clicked");
+    var playPauseButton = document.getElementById('play-pause-button');
+    const playPauseButtonState = playPauseButton.textContent.trim();
+    if (playPauseButtonState === 'Play') {
+        playPauseButton.textContent = 'Pause';
+        playPauseButton.innerHTML = '<i class="fas fa-pause"></i> Pause';
+    } else {
+        playPauseButton.textContent = 'Play';
+        playPauseButton.innerHTML = '<i class="fas fa-play"></i> Play';
+    }
+  };
+};
+
 var sliders = document.getElementsByClassName('slider');
 for (var i = 0; i < sliders.length; i++) {
     sliders[i].oninput = function() {
@@ -6,19 +51,4 @@ for (var i = 0; i < sliders.length; i++) {
     }
     // Trigger the event manually to display the initial value
     sliders[i].oninput();
-}
-
-function togglePlayPause() {
-    console.log("play/pause button clicked");
-    var playPauseButton = document.getElementById('play-pause-button');
-    const playPauseButtonState = playPauseButton.textContent.trim();
-    if (playPauseButtonState === 'Play') {
-        // Play the music
-        playPauseButton.textContent = 'Pause';
-        playPauseButton.innerHTML = '<i class="fas fa-pause"></i> Pause';
-    } else {
-        // Pause the music
-        playPauseButton.textContent = 'Play';
-        playPauseButton.innerHTML = '<i class="fas fa-play"></i> Play';
-    }
 }
