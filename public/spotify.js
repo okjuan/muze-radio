ce561fa2bf460c323b4ed98a07f0f75829d21714
okjuan <jcgallegdup@gmail.com>
2024-01-12 17:36:56 -0800

    Replace hard-coded number with global variable

--- public/spotify.js
+++ public/spotify.js
@@ -11,6 +11,7 @@ const clientId = '8efedd3d29214978b2a0e6e63444974b';
 export const spotifySeedGenres = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"];
 export const maxPlaylistsPerRequest = 50;
 const maxSongsPerAddToPlaylistRequest = 100;
+const maxSongsPerGetRecommendationsRequest = 100;
 export const maxSeedGenres = 5;
 var userPlaylists = undefined;
 var userId = undefined;
@@ -165,7 +166,7 @@ export function addSongsToPlaylist(playlistId, songUris, position=undefined) {
 }
 
 export function getRecommendations(audioFeatures, genres) {
-    var queryParams = `seed_genres=${genres.join(',')}&limit=100&market=${MARKET}&`
+    var queryParams = `seed_genres=${genres.join(',')}&limit=${maxSongsPerGetRecommendationsRequest}&market=${MARKET}&`
     queryParams += Object.entries(audioFeatures)
         .map(([key, value]) => `target_${key}=${value}`)
         .join('&');
