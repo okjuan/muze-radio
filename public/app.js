5258013f1498e07bdc845867cea720b0d666bfc0
okjuan <jcgallegdup@gmail.com>
2024-01-19 17:55:46 -0800

    Handle case when no audio features are found

--- public/app.js
+++ public/app.js
@@ -289,7 +289,7 @@ function addSpotifyLinksToArtistNames(artists) {
 
 function updateCurrentlyPlayingAudioFeatures(current_track) {
     getSpotifyAudioFeatures([current_track.id]).then((audioFeatures) => {
-        if (audioFeatures.length < 1) {
+        if (audioFeatures.length < 1 || !audioFeatures[0]) {
             console.error("No audio features found for track " + current_track.id);
             document.querySelectorAll('.audio-feature-icon-currently-playing').forEach(icon => icon.style.display = 'none');
             return;
